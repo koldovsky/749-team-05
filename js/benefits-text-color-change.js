66493e35d170e6efcbbf312bfe7445989e081851
marianamartsy <123500008+marianamartsy@users.noreply.github.com>
2023-02-27 11:54:18 +0000

    benefits.js edited

--- js/benefits-text-color-change.js
+++ js/benefits-text-color-change.js
@@ -1,12 +1,12 @@
 (function () {
 	const benefitsText = document.querySelectorAll('.benefits__card-text')
 
-	const changeColor = event => {
+	const randomColor = changer => {
 		const r = Math.floor(Math.random() * 255)
 		const g = Math.floor(Math.random() * 255)
 		const b = Math.floor(Math.random() * 255)
 
-		event.target.style.color = `rgb(${r},${g},${b})`
+		changer.target.style.color = `rgb(${r},${g},${b})`
 	}
 
 	const colourDefault = change => {
@@ -14,10 +14,7 @@
 	}
 
 	benefitsText.forEach(text => {
-    text.addEventListener('pointerenter', changeColor)
+    text.addEventListener('pointerenter', randomColor)
     text.addEventListener('pointerleave', colourDefault)
-    text.addEventListener('touchstart', changeColor)
-    text.addEventListener('touchend', colourDefault)
 	})
-
 })()
