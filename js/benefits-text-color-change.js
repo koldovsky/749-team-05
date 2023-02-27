4e11aaeaa2f392510b8d27c8105a4021bb2d958b
marianamartsy <123500008+marianamartsy@users.noreply.github.com>
2023-02-27 10:52:22 +0000

    benefits-text-color-change.js added

---
+++ js/benefits-text-color-change.js
@@ -0,0 +1,22 @@
+(function () {
+	const benefitsText = document.querySelectorAll('.benefits__card-text')
+
+	const changeColor = event => {
+		const r = Math.floor(Math.random() * 255)
+		const g = Math.floor(Math.random() * 255)
+		const b = Math.floor(Math.random() * 255)
+
+		event.target.style.color = `rgb(${r},${g},${b})`
+	}
+
+	const colourDefault = change => {
+		change.target.style.color = ''
+	}
+
+	benefitsText.forEach(text => {
+		text.addEventListener('mouseenter', changeColor)
+	})
+	benefitsText.forEach(text => {
+		text.addEventListener('mouseleave', colourDefault)
+	})
+})()
