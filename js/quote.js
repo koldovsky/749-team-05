e618f03e9b6ad2d6ce4e57b18bf05cf03541b5e1
NataliaNevidoma <56003837+NataliaNevidoma@users.noreply.github.com>
2023-03-08 19:44:50 +0200

    Changed quote.js

--- js/quote.js
+++ js/quote.js
@@ -1,17 +1,22 @@
 (function () {
-    let changeName = [
-        'Lorry Morengo, CEO',
-        'Nazar Klimovych, Team Leader of team - 05',
-    ]
+  const name = document.querySelector(".author");
+  const text = document.querySelector(".quote__text");
 
-    function getChanging() {
-        const index = Math.floor(Math.random() * changeName.length);
-        return changeName[index];
-    }
-    function takeName() {
-        const name = document.querySelector('.author');
-        name.innerHTML = getChanging();
-    }
+  name.addEventListener("mouseover", () => {
+    name.innerHTML = "Benjamin Franklin";
+  });
 
-    setInterval(takeName, 2000)
+  name.addEventListener("mouseout", () => {
+    name.innerHTML = "Lorry Morengo, CEO";
+  });
+
+  text.addEventListener("mouseover", () => {
+    text.innerHTML =
+      '"It is a grand mistake to think of being great without goodness and I pronounce it as certain that there was never a truly great man that was not at the same time truly virtuou"';
+  });
+
+  text.addEventListener("mouseout", () => {
+    text.innerHTML =
+      '"I think the people that end up doing well here are just generally passionate about being owners of their space and making the world a better place."';
+  });
 })();
