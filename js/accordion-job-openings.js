d7ebd362b8f5a9eac192c89b07c1829b437b2ee1
Nataliia Voronchak <80248266+NatalieVoronchak3107@users.noreply.github.com>
2023-03-09 09:27:45 +0000

    revised js in accordion

--- js/accordion-job-openings.js
+++ js/accordion-job-openings.js
@@ -1,18 +1,23 @@
 (function () {
-    const accordionItemHeaders = document.querySelectorAll(
-      ".accordion-item__header"
-    );
-  
-    accordionItemHeaders.forEach((accordionItemHeader) => {
-      accordionItemHeader.addEventListener("click", (event) => {
-        accordionItemHeader.classList.toggle("active");
-        const accordionItemBody = accordionItemHeader.nextElementSibling;
-        if (accordionItemHeader.classList.contains("active")) {
-          accordionItemBody.style.maxHeight =
-            accordionItemBody.scrollHeight + "px";
-        } else {
-          accordionItemBody.style.maxHeight = 0;
-        }
-      });
+  const accordionItemHeaders = document.querySelectorAll(
+    ".accordion-item__header"
+  );
+
+  accordionItemHeaders.forEach((accordionItemHeader) => {
+    accordionItemHeader.addEventListener("click", event => {
+      const currentlyActiveAccordionItemHeader = document.querySelector(".accordion-item__header.active");
+      if (currentlyActiveAccordionItemHeader && currentlyActiveAccordionItemHeader !== accordionItemHeader) {
+        currentlyActiveAccordionItemHeader.classList.toggle("active");
+        currentlyActiveAccordionItemHeader.nextElementSibling.style.maxHeight = 0;
+      }
+      accordionItemHeader.classList.toggle("active");
+      const accordionItemBody = accordionItemHeader.nextElementSibling;
+      if (accordionItemHeader.classList.contains("active")) {
+        accordionItemBody.style.maxHeight =
+          accordionItemBody.scrollHeight + "px";
+      } else {
+        accordionItemBody.style.maxHeight = 0;
+      }
     });
-  })();
+  });
+})();
